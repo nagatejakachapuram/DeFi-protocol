6bf77073221ab632b95ab8994b0369bc22e318fb
Nagateja kachapuram <104291208+nagatejakachapuram@users.noreply.github.com>
2025-01-08 20:02:27 +0530

    Update DSCEngine.sol
    added mint functions

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.28;
 import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 /**
  * @title DSCEngine
@@ -35,8 +36,16 @@ contract DSCEngine is ReentrancyGuard {
     ////////////////////////////
     //     State Variables    //
     ///////////////////////////
+    uint256 private constant ADDITIONAL_FEED_PRECISION = 1e10;
+    uint256 private constant PRECISION = 1e18;
+
     mapping(address token => address priceFeed) private s_priceFeeds; // tokenToPricefeed
     mapping(address user => mapping(address token => uint256 amount)) private s_collateralDeposited;
+    mapping(address user => uint256 amountDscMinted) private s_DSCMinted;
+    address[] private s_collateralTokens;
+
+    address weth;
+    address wbtc;
 
     DecentralizedStableCoin private immutable i_dsc;
 
@@ -73,6 +82,7 @@ contract DSCEngine is ReentrancyGuard {
         }
         for (uint256 i = 0; i < tokenAddresses.length; i++) {
             s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+            s_collateralTokens.push(tokenAddresses[i]);
         }
         i_dsc = DecentralizedStableCoin(dscAddress);
     }
@@ -102,11 +112,70 @@ contract DSCEngine is ReentrancyGuard {
 
     function redeemCollateral() external {}
 
-    function mintDSC() external {}
+    function mintDSC(uint256 amountDscToMint) external moreThanZero(amountDscToMint) nonReentrant {
+        s_DSCMinted[msg.sender] += amountDscToMint;
+        // If they minted too much ($150 DSC, $100 ETH)
+        revertfHealthFatorIsBroken(msg.sender);
+    }
 
     function burnDSC() external {}
 
     function liquidate() external {}
 
     function getHealthFactor() external view {}
+
+
+    //////////////////////////////////////
+    //    Private & Internal Functions  //
+    /////////////////////////////////////
+
+    function _getAccountInformation(address user) private view returns(uint256 totalDscMinted, uint256 collateralValueInUsd){
+        totalDscMinted = s_DSCMinted[user];
+        collateralValueInUsd = getAccountCollateralValue(user);
+    }
+
+    /**
+     * 
+     * Returns how close to liquidation a user is
+     * If a user goes below 1, then they get liquidated.
+     */
+
+    function _healthFactor(address user) private view returns(uint256) {
+        // Total collateral value > total DSC minted
+        (uint256 totalDscMinted, uint256 collateralValueInUsd) = _getAccountInformation(user);
+
+    }
+
+    function _revertfHealthFatorIsBroken(address user) internal view {
+        // 1. Check health factor (do they have enough collateral)
+        // 2. Revert if they don't
+
+
+    }
+
+
+    //////////////////////////////////////
+    //    Public & Internal Functions  //
+    /////////////////////////////////////
+
+    function getAccountCollateralValue(address user) public view returns(uint256 totalCollateralValueInUsd){
+        // loop through each collateral token, get the amount they deposited, and map it to
+        // price, to get the USD value.
+        for (uint256 i = 0; i < s_collateralTokens.length; i++){
+            address token = s_collateralTokens[i];
+            uint256  amount = s_collateralDeposited[user][token];
+            totalCollateralValueInUsd += getUsdValue(token, amount);
+        }
+        return totalCollateralValueInUsd;
+    }
+
+    function getUsdValue(address token, uint256 amount) public view returns(uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[token]);
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        // 1 ETH = $1000
+        // The retunred value from CL will be 1000  1e18
+        return (uint256(price) * ADDITIONAL_FEED_PRECISION * amount) / PRECISION; // (1000 * 1e8 * (1e10)) * 1000 * 1e18
+
+
+    }
 }
